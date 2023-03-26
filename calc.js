8d2f8ff2cc7950de509b47bea30e3346ce156f14
leoaraujo92 <leonardo.araujo2008@gmail.com>
2023-03-26 17:24:29 -0300

    Definindo a data

--- calc.js
+++ calc.js
@@ -18,9 +18,48 @@ function apagar(){
 
 
 function showHour(){
-    if(month == 2){
-        month = 03
-    }
+   
+    switch(month){
+        case 0: 
+            month = "Jan"
+            break
+        case 1: 
+            month = "Fev"
+            break
+        case 2: 
+            month = "Mar"
+            break
+        case 3:
+            month = "Abr"
+            break
+        case 3:
+            month = "Abr"
+            break
+        case 4:
+            month = "Mai"
+            break
+        case 5:
+            month = "Jun"
+            break
+        case 6:
+            month = "Jul"
+            break
+        case 7:
+            month = "Ago"
+            break
+        case 8:
+            month = "Set"
+            break
+        case 9:
+            month = "Out"
+            break
+        case 10:
+            month = "Nov"
+            break
+        case 11:
+            month = "Dez"
+            break
+        }
     displayHour.innerHTML = `${hour}:${min}`
     displayDate.innerHTML = `${day}/${month}/${year}`
     
