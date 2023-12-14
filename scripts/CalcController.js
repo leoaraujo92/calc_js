51c724b8a4b9d4a3bf1dc810b681c1272f5fbe14
leoaraujo92 <leonardo.araujo2008@gmail.com>
2023-12-14 14:06:11 -0300

    Criando metodos addEventListener

--- scripts/CalcController.js
+++ scripts/CalcController.js
@@ -51,25 +51,70 @@ class CalcController {
     // FIM DA FUNÇÃO INITIALIZE
 
 
+    /* ---------------------------------------------EVENTO DE CLICK-------------------------------------------
+    //EVENTO DE CLICK 
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#base > div#list-1 > input, div#list-2 > input, div#list-3 > input, div#list-4 > input, div#list-5 > input")
 
         console.log(buttons)
 
-        /*buttons.addEventListener('click', e=>{
+        buttons.addEventListener('click', e=>{
             console.log(e)
-        })*/
-
-        buttons.forEach(btn=>{
-
-            btn.addEventListener('click', e => {
-                console.log(e)
-            })
         })
 
+        buttons.forEach((btn, index)=>{
+
+            btn.addEventListener('click', e => {
+
+                console.log(btn.className.replace("bnt-",""))
+            })
+        })
+    }-----------------------------------------------FIM EVENTO CLICK--------------------------------------------*/
+
+
+
+    /*---------------------------------------------CRIANDO UMA MÉTODO DE EVENTLISTENER---------------------------*/
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+        })
 
     }
+    /**-----------------------------------------------FIM LISTA DE EVENTOS-----------------------------------------*/
+
+    /**
+     * initButtonsEvents usa o addEventListernerAll para criar multiplos eventos
+     * Que neste caso é o evento de click e drag
+     */
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#base > div#list-1 > input, div#list-2 > input, div#list-3 > input, div#list-4 > input, div#list-5 > input")
+
+        console.log(buttons)
+
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn, "click drag", e => {
+
+                console.log(btn.className.replace("bnt-",""))
+            })
+
+    /**---------------------------------------------MUDANDO O CURSOR DO MOUSE VIA JS---------------------------------- */
+    /**
+     * Criei este método com css, porém é possivel e muito menos trabalhoso criar para varios elementos com JavaScript
+     * Usuando o método addEventListenerAll criando acima
+     */
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+
+                btn.style.cursor = "pointer"
+            })
+
+        })
+    }
+
 
             //CRIANDO METODO PARA RETIRAR O ATRASO DO CARREGAMENTO DA DATA E HORA NO DISPLAY
             setDisplayDateTime(){
